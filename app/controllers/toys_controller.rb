213551597849e4ecdddd9804ad2d077050d9d7cd
Pierce <piercedavidraphael@gmail.com>
2021-12-22 09:51:25 -0600

    Completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,25 +7,28 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    # byebug
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = Toy.find(params[:id])
     toy.update(toy_params)
+    # byebug
+    render json: toy, status: :accepted
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
+    toy = Toy.find(params[:id])
+    destroyed_toy = toy.destroy
+    render json: destroyed_toy, status: :no_content
   end
 
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
   end
 
 end
